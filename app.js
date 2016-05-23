7e807e1d43634725ab518b26b86e1b9833882897
Ryder Bergerud <ryder.bergerud@gmail.com>
2016-05-23 15:23:21 -0700

    just completed tweeting streams

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var fs = require("fs"),
     app = require("http").createServer(handler),
     io = require("socket.io").listen(app, {log: false}),
     theport = process.env.PORT || 2000;
+    twitter = require("ntwitter"),
+    keys = require('./keys.json');
 
 app.listen(theport);
 
@@ -27,3 +29,58 @@ io.sockets.on("connection", function(socket) {
 
   socket.emit("something else", {hello: "Hello, are you connected?"})
 })
+
+/**
+* @var tw the Twitter Streaming API initalization
+* @var stream When a stream is created this will be the only instance of it
+* @var track The tracking words for the stream
+* @var users An array of connected users to the application
+*/
+
+var tw = new twitter({
+    consumer_key: keys.consumer_key,
+    consumer_secret: keys.consumer_secret,
+    access_token_key: keys.access_token_key,
+    access_token_secret: keys.access_token_secret
+  }),
+  stream = null,
+  track = "venezuala",
+  users = [];
+
+// A listener for a client connection
+io.sockets.on("connection", function(socket) {
+  //The user should be added to the array if it doesn't exist
+  if(user.indexOf(socket.id) === -1) {
+    users.push(socket.id);
+  }
+
+  //log
+  logConnectedUsers();
+
+  socket.on("start stream", function() {
+    if(stream === null) {
+      tw.stream("statuses/filter", {
+        track: track
+      }, function(s) {
+        stream = s;
+        stream.on("data", function(data) {
+          // Only broadcast when users are online
+          if(users.length > 0) {
+            socket.broadcast.emit("new tweet", data);
+            socket.emit("new tweet", data);
+          } else {
+            // If there are no users connected we destroy the stream
+            stream.destroy();
+            stream = null;
+          }
+        })
+      })
+    }
+  })
+})
+
+function logConnectedUsers() {
+  console.log("=============== CONNECTED USERS ===================");
+  console.log("== :: " + users.length);
+  console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+}
