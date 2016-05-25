6ef1d6e72f662ea14a1b3d340e6fa81e63a5ad2e
Ryder Bergerud <ryder.bergerud@gmail.com>
2016-05-25 15:02:54 -0700

    set up to write elements to DOM from each example of reactive programming

--- public/script.js
+++ public/script.js
@@ -1,31 +1,65 @@
 var _socket = null;
 $(function() {
-  var site = document.URL;
-    if(io !== undefined) {
-        // Here you create the connection with the server
-        _socket = io.connect(site);
-        // This will listen when the server emits the "connected" signal
-        // informing to the client that the connection has been stablished
-        _socket.on("connected", function(r) {
-            $("head").find("title").html("Tracking now: " + r.tracking);
-            $(".tracking").html(r.tracking);
+  if(io !== undefined) {
+    var tweetObs = startStream();
+    tweetObs.subscribe(displayTweet, logError);
 
-            // Here the client tells the server to "start stream"
-            emitMsj("start stream");
-        });
+    //Defer
+    var interval = Rx.Observable.interval(1000);
+    // interval.subscribe(x => console.log(x));
 
-        var tweetObs = Rx.Observable.create(function (observer) {
-          _socket.on("new tweet", function(t) {
-            observer.onNext(t);
-          });
-          _socket.on("error", function(e) {
-            observer.onError(e);
-          })
-        });
+    //Empty/Never/Throw
 
-        tweetObs.subscribe(displayTweet, logError);
-    }
+    //From
+
+  }
 });
+
+// @param Parent is jquery element
+function StreamElement(parent, operatorName) {
+  var segment = $('<div>').addClass('ui segment ' + operatorName)
+  var header  = $('<div>').addClass('ui header').html(operatorName)
+  var grid    = $('<div>').addClass('ui raised segments columns very relaxed grid').attr('id', operatorName)
+
+  segment.append(header).append(grid);
+  parent.append(segment);
+  this.grid = $('#' + operatorName);
+
+  this.addNext = function (text) {
+    var pointer = $('<div>').addClass('ui vertical divider')
+    var column = $('<div>').addClass('ui segment column')
+                            .html(text)
+                            .append($('<i>').addClass('long arrow right icon'))
+    console.log(column);
+    console.log(this.grid.html());
+    this.grid.append(column);
+  }
+}
+
+function startStream() {
+    // Here you create the connection with the server
+    _socket = io.connect(document.URL);
+    // This will listen when the server emits the "connected" signal
+    // informing to the client that the connection has been stablished
+    _socket.on("connected", function(r) {
+        $("head").find("title").html("Tracking now: " + r.tracking);
+        $(".tracking").html(r.tracking);
+
+        // Here the client tells the server to "start stream"
+        emitMsj("start stream");
+    });
+
+    var tweetObs = Rx.Observable.create(function (observer) {
+      _socket.on("new tweet", function(t) {
+        observer.onNext(t);
+      });
+      _socket.on("error", function(e) {
+        observer.onError(e);
+      })
+    });
+
+    return tweetObs;
+}
 
 function displayTweet(t) {
     var pic = $('<img>').attr({src: t.user.profile_image_url})
